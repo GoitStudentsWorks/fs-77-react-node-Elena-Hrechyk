3c0a05d267c49ed8aaa08033b154b0edd4ad3600
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-21 10:48:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-project-ITSide-DrinkMaster">
+        <BrowserRouter basename="/fs-77-react-node-Elena-Hrechyk">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
